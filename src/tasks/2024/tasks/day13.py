3e28e281b5a0d7a0bba168365a0eb740a64bc124
a.gorbachev <arsenii.gorbachev1@gmail.com>
2025-11-25 20:05:30 +0500

    day 13.1 fix fast fail func, measure speedup

--- src/tasks/2024/tasks/day13.py
+++ src/tasks/2024/tasks/day13.py
@@ -1,14 +1,28 @@
 import re
 from dataclasses import dataclass
+from fractions import Fraction
+from functools import wraps
+from timeit import Timer
 
 from src.utils.logger import get_logger
 from src.utils.position import Position2D
-from src.utils.test_and_run import test
+from src.utils.test_and_run import run, test
 
 _logger = get_logger()
 
 _button_rexp = re.compile(r"Button [AB]: X([-+]\d+), Y([-+]\d+)")
 _prize_rexp = re.compile(r"Prize: X=(\d+), Y=(\d+)")
+
+
+def timeit_deco(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t = Timer(lambda: func(*args, **kwargs))
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} took {t.timeit(number=1):.6f}s")
+        return result
+
+    return wrapper
 
 
 @dataclass
@@ -39,7 +53,21 @@ def _parse_input(inp: list[str]) -> list[Machine]:
     return machines
 
 
-def get_point_of_intersection(machine: Machine) -> Position2D | None:
+def fast_fail(machine: Machine, max_tokens: int) -> bool:
+    """
+    Checks if prize x and prize y is even reachable by spending no more than {max_tokens} tokens
+    """
+    for axis in (0, 1):
+        max_pos = max_tokens * (machine.button_a[axis] + machine.button_b[axis])
+        if max_pos < machine.prize[axis]:
+            _logger.debug(
+                f"Fast fail on {axis=}: {max_tokens=} * ({machine.button_a[axis]=} + {machine.button_b[axis]=}) = {max_pos} < {machine.prize[axis]=}"
+            )
+            return True
+    return False
+
+
+def get_tokens_to_pay(machine: Machine) -> int:
     """
     The main idea:
     we need to combine two vectors a and b to get prize.
@@ -67,7 +95,7 @@ def get_point_of_intersection(machine: Machine) -> Position2D | None:
     All other cases not works for us, return None
 
     Returns:
-        Pos
+        int
 
     """
     a = machine.button_a
@@ -89,24 +117,40 @@ def get_point_of_intersection(machine: Machine) -> Position2D | None:
     else:
         _logger.debug(f"{x=}, {diff_to_int=} bad =(")
 
-    # now recheck
-    a_clicks = 80
-    b_clicks = 40
+    # now let's use Cramer rule to minimize fractions at slope calculation
+    ax, ay = machine.button_a.x, machine.button_a.y
+    bx, by = machine.button_b.x, machine.button_b.y
+    px, py = machine.prize.x, machine.prize.y
 
-    a_x = a.x * a_clicks
-    b_x = b.x * b_clicks
-    expected_x = a_x + b_x
+    # Determinant of the 2x2 matrix [[ax, bx], [ay, by]]
+    d = ax * by - ay * bx
 
-    a_y = a.y * a_clicks
-    b_y = b.y * b_clicks
-    expected_y = a_y + b_y
+    if d == 0:
+        _logger.debug(f"{d=}: Parallel / no unique solution")
+        return 0
 
-    if a == (94, 34) and b == (22, 67) and machine.prize == (8400, 5400):
-        assert machine.prize == (expected_x, expected_y)
-    return x
+    # Cramer's rule
+    na = Fraction(px * by - py * bx, d)
+    nb = Fraction(ax * py - ay * px, d)
+
+    # Check that point coordinates are integers
+    if na.denominator != 1 or nb.denominator != 1:
+        _logger.debug(f"some of {na.denominator=} or {nb.denominator=} are fractions!")
+        return 0
+
+    na_int = na.numerator
+    nb_int = nb.numerator
+
+    if na_int < 0 or nb_int < 0:
+        _logger.debug(f"some of {na_int=} or {nb_int=} are negative!")
+        return 0
+
+    res = na_int * machine.button_a_price + nb_int * machine.button_b_price
+    _logger.debug(f"{res=}")
+    return res
 
 
-def calculate_tokens_to_prize(machine: Machine) -> int:
+def calculate_tokens_to_prize(machine: Machine, max_tokens: int, use_fast_fail: bool = True) -> int:
     a_slope = machine.button_a.slope()
     b_slope = machine.button_b.slope()
     prize_slope = machine.prize.slope()
@@ -125,29 +169,22 @@ def calculate_tokens_to_prize(machine: Machine) -> int:
     msg += f" {prize_slope=}. full state: {machine.button_a=}, {machine.button_b=}, {machine.prize=}"
     _logger.debug(msg)
 
-    get_point_of_intersection(machine)
+    if use_fast_fail and fast_fail(machine, max_tokens):
+        return 0
 
-    tokens = 0
-    return tokens
-
-
-def task(inp: list[str], task_num: int):
-    machines = _parse_input(inp)
-    total_tokens = 0
-    for machine in machines:
-        tokens = calculate_tokens_to_prize(machine)
-        total_tokens += tokens
-    _logger.debug("ask finished")
+    return get_tokens_to_pay(machine)
 
 
-def task1(inp):
-    return task(inp, 1)
+def task(inp: list[str], task_num: int, max_tokens: int, use_fast_fail):
+    return sum(calculate_tokens_to_prize(machine, max_tokens, use_fast_fail) for machine in _parse_input(inp))
 
 
-def task2(inp):
-    return task(inp, 2)
+@timeit_deco
+def task1(inp, use_fast_fail=True):
+    return task(inp, 1, max_tokens=100, use_fast_fail=use_fast_fail)
 
 
 if __name__ == "__main__":
-    test(task1, 280)
-    # run(task1)
+    test(task1, 480)
+    assert run(task1, True) > 18025
+    assert run(task1, False) > 18025
