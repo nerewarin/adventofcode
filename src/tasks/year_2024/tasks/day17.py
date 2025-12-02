9013446afcf65eb0e148ddd0f6b3bcc80f8d9492
a.gorbachev <arsenii.gorbachev1@gmail.com>
2025-12-02 08:39:14 +0500

    2024/day17.2 semi-manual research, but I'm proud pf this Sisyphean labor a lot!!

--- src/tasks/year_2024/tasks/day17.py
+++ src/tasks/year_2024/tasks/day17.py
@@ -10,6 +10,10 @@ from src.utils.test_and_run import run
 _logger = get_logger()
 
 
+def pretty_int(n: int) -> str:
+    return f"{n:,}".replace(",", "_")
+
+
 class ChronospatialComputer:
     def __init__(self, program, a=0, b=0, c=0):
         self.a, self.b, self.c = a, b, c
@@ -184,7 +188,7 @@ def task1(inp, **kw):
     return task(inp, **kw)
 
 
-def task2(inp):
+def task2(inp, min_a=None, max_a=None, delta=None, matches_to_print=None, counter_to_pint_matches=1_000):
     """
     Now we need to debug program in reverse.
     consider input from run:
@@ -245,76 +249,96 @@ def task2(inp):
     then  B % 8 -> output must be 3 now: so B = xxx011
 
     """
-    a = 0
-    found = float("inf")
-
     _initial_computer = ChronospatialComputer.from_multiline_input(inp, 1)
     program = _initial_computer.program
+
     expected_len = len(program)
 
-    prior_matches = 0
-    diff = 0
     counter = 0
     matches = 0
-    already_found_full_len = False
-    min_a = None
-    while a < found:
+
+    a = min_a
+    _logger.info(
+        f"searching for A between min_a={pretty_int(min_a)} and max_a={pretty_int(max_a)}: delta={max_a - min_a}"
+    )
+    while a < max_a:
         computer = ChronospatialComputer.from_multiline_input(inp, 1)
         computer.a = a
         computer.run_program()
 
         output = computer.output
-        if output == computer.program:
-            found = a
-            _logger.info(f"found {a=}")
-
         output_len = len(output)
-        if output_len == expected_len:
-            already_found_full_len = True
 
-            matches = 0
+        if output == computer.program:
+            # found = a
+            _logger.info(f"found {a=}")
+            return a
+
+        matches = 0
+        if output_len == expected_len:
+            # already_found_full_len = True
+
             for i in range(expected_len):
                 if output[-i - 1] == program[-i - 1]:
                     matches += 1
                 else:
                     break
-            min_step = (expected_len - matches) ** 10
+        #     min_step = (expected_len - matches) ** 10
+        #     if min_step:
+        #         import random
+        #         entropy = random.randrange(1, 11)
+        #         min_step = min_step * entropy // 10
+        #
+        #     if matches < (prior_matches - 1):
+        #         delta = -(max(delta // 2, min_step))
+        #     elif matches > prior_matches:
+        #         delta = max(delta // 2, min_step)
+        #     else:
+        #         next_digit_to_find = program[-matches - 1]
+        #         our_digit = output[-matches - 1]
+        #         if our_digit < next_digit_to_find:
+        #             delta = abs(delta)
+        #         elif our_digit > next_digit_to_find:
+        #             delta = -abs(delta)
+        #         else:
+        #             raise RuntimeError()
+        #
+        #     prior_matches = matches
+        #
+        # elif output_len < expected_len:
+        #     if already_found_full_len:
+        #         if min_a is None:
+        #             min_a = a
+        #         else:
+        #             min_a = max(min_a, a)
+        #     delta = (a or 1) * 2
+        # else:
+        #     delta = int(-(a or 1) * 0.1)
 
-            if matches < (prior_matches - 1):
-                diff = -(max(diff // 2, min_step))
-            elif matches > prior_matches:
-                diff = max(diff // 2, min_step)
-            else:
-                next_digit_to_find = program[-matches - 1]
-                our_digit = output[-matches - 1]
-                if our_digit < next_digit_to_find:
-                    diff = abs(diff)
-                elif our_digit > next_digit_to_find:
-                    diff = -abs(diff)
-                else:
-                    raise RuntimeError()
+        # delta += 1000
+        delta = delta or 10_000
+        if matches_to_print and matches >= matches_to_print and not counter % counter_to_pint_matches:
+            _logger.info(
+                f"matches! {counter=}. a={pretty_int(a)}, {output=}, {output_len=}, {expected_len=}, {delta=}. {matches=}"
+            )
+        elif not counter % 10_000:
+            _logger.info(
+                f"{counter=}. a={pretty_int(a)}, {output=}, {output_len=}, {expected_len=}, {delta=}. {matches=}"
+            )
 
-            prior_matches = matches
-
-        elif output_len < expected_len:
-            if already_found_full_len:
-                if min_a is None:
-                    min_a = a
-                else:
-                    min_a = max(min_a, a)
-            diff = (a or 1) * 2
-        else:
-            diff = int(-(a or 1) * 0.1)
-
-        if not counter % 10_001:
-            _logger.info(f"{counter=}. {a=}, {output=}, {output_len=}, {expected_len=}, {diff=}. {matches=}")
-
-        if min_a is None:
-            a += diff
-        else:
-            a = min(a + diff, min_a + 1)
+        a += delta
+        # if min_a is None:
+        #     a += delta
+        # else:
+        #     if output_len < expected_len:
+        #         a = min(a + delta, min_a + 1)
         counter += 1
-    return True
+
+    _logger.info(
+        f"lastly, {counter=}. a={pretty_int(a)}, {output=}, {output_len=}, {expected_len=}, {delta=}. {matches=}"
+    )
+    _logger.info(f"(False) Done searching for A between {min_a=} and {max_a=}: delta={max_a - min_a}")
+    return False
 
 
 class UnitTest:
@@ -386,9 +410,120 @@ if __name__ == "__main__":
     # UnitTest.test5()
     #
     # test(task1, "4,6,3,5,6,3,5,2,1,0")
-    # run(task1)
+    run(task1)
     #
     # UnitTest.test6()
     # test(task2, True)
     # test(task2, True, test_part=2)
-    run(task2)
+
+    # well, for the task2 I've just used brute-force to estimate how output growths, then picked up ranges of A for
+    # which output is at least needed length and then used gradient descent
+
+    # First, I did full run from sublime to find at least len_putput match, see day_17_research.txt
+    # run(task2, min_a=237_755_333_196_000, max_a=287_755_333_196_000, delta=100_000_000, matches_to_print=5)  # full matches
+
+    # here are some insights from it...
+    # 5 matches
+    # run(task2, min_a=216580542908000, max_a=216600542908000)
+    # run(task2, min_a=215250542908000, max_a=216250542908000)
+
+    # 7 matches
+    # run(task2, min_a=216591542908000, max_a=216593542908000)
+
+    # 9 matches
+    # run(task2, min_a=216592042908000, max_a=216592072908000, delta=1000)
+    # run(task2, min_a=216592982908000, max_a=216593012908000, delta=1000)
+
+    # run(task2, min_a=216592042908000, max_a=216592062908000, delta=100)
+    # run(task2, min_a=216592982908000, max_a=216592995908000, delta=100)
+
+    # run(task2, min_a=216592049908000, max_a=216592053908000, delta=10)
+    # run(task2, min_a=216592989908000, max_a=216592992908000, delta=10)
+    #
+    # run(task2, min_a=216592050808000, max_a=216592053008000, delta=5)
+    # run(task2, min_a=216592990308000, max_a=216592992508000, delta=5)
+
+    # but I haven't found anything in these regions...so I run it again with more precise steps not to miss smth...
+    # from this I got new regions:
+    # 1. matches=5
+    # 216550542908000...216650542908000
+    # 234150542908000...234250542908000
+    # run(task2, min_a=215_250_542_908_000, max_a=237_755_333_196_000, delta=10_000_000, matches_to_print=5)
+    # 2. matches = 4
+    # 216050542908000 ,,,216250542908000
+    # 218250542908000 ,,,218450542908000
+    # 233650542908000 ,,,233850542908000
+    # 235850542908000 ..236050542908000
+    # 236450542908000 .. 236650542908000
+
+    # 1. inspect 5 matches
+    # run(task2, min_a=216550542908000, max_a=216650542908000, delta=1_000_000, matches_to_print=6)
+    # run(task2, min_a=234150542908000, max_a=234250542908000, delta=1_000_000, matches_to_print=6)
+    # inspect 6 matches from this
+    # run(task2, min_a=216_590_542_908_000, max_a=216_600_542_908_000, delta=100_000, matches_to_print=7)
+    # run(task2, min_a=234_180_542_908_000, max_a=234_190_542_908_000, delta=100_000, matches_to_print=7)
+    # inspect 7 matches from this
+    # run(task2, min_a=216_591_542_908_000, max_a=216_593_542_908_000, delta=10_000, matches_to_print=8)
+    # run(task2, min_a=234_183_542_908_000, max_a=234_185_542_908_000, delta=10_000, matches_to_print=8)
+    # inspect 8 matches from this
+    # run(task2, min_a=216_592_042_908_000, max_a=216_592_142_908_000, delta=1_000, matches_to_print=9)
+    # run(task2, min_a=216_592_942_908_000, max_a=216_593_042_908_000, delta=1_000, matches_to_print=9)
+    # run(task2, min_a=234_184_142_908_000, max_a=234_184_342_908_000, delta=1_000, matches_to_print=9)
+    # run(task2, min_a=234_185_142_908_000, max_a=234_185_242_908_000, delta=1_000, matches_to_print=9)
+    # inspect 9 matches from this
+    # run(task2, min_a=216_592_042_908_000, max_a=216_592_062_908_000, delta=100, matches_to_print=10)
+    # run(task2, min_a=216_592_982_908_000, max_a=216_593_002_908_000, delta=100, matches_to_print=10)
+    # run(task2, min_a=234_184_232_908_000, max_a=234_184_242_908_000, delta=100, matches_to_print=10)
+    # run(task2, min_a=234_185_172_908_000, max_a=234_185_182_908_000, delta=100, matches_to_print=10)
+    # only 2 sections have 10 matches from this, pick best from each
+    # run(task2, min_a=216_592_049_908_000, max_a=216_592_053_908_000, delta=10, matches_to_print=10)
+    # run(task2, min_a=216_592_989_908_000, max_a=216_592_992_908_000, delta=10, matches_to_print=10)
+    # run(task2, min_a=216_592_995_908_000, max_a=216_592_996_908_000, delta=10, matches_to_print=10)
+    # run(task2, min_a=234_184_235_908_000, max_a=234_184_239_908_000, delta=10, matches_to_print=10)
+    # run(task2, min_a=234_185_175_908_000, max_a=234_185_178_908_000, delta=10, matches_to_print=10)
+    # run(task2, min_a=234_185_179_908_000, max_a=234_185_182_408_000 + 1000, delta=10, matches_to_print=10)
+    # again, narrow the sectors
+    # run(task2, min_a=216_592_050_808_000, max_a=216_592_052_908_000, delta=5, matches_to_print=10) # only 9 there
+    # run(task2, min_a=216_592_990_308_000, max_a=216_592_992_508_000, delta=5, matches_to_print=10) # only 9 there
+    #
+    # run(task2, min_a=216_592_996_168_000, max_a=216_592_996_208_000, delta=1, matches_to_print=10) # more hopes on that, even 11 found!
+    # run(task2, min_a=216_592_996_008_000, max_a=216_592_996_408_000, delta=5, matches_to_print=10) # same section wider
+    #
+    # run(task2, min_a=234_184_236_808_000, max_a=234_184_238_908_000, delta=5, matches_to_print=10) # only 9 there
+    #
+    # run(task2, min_a=234_185_176_308_000, max_a=234_185_178_508_000, delta=5, matches_to_print=10) # only 9 there
+    #
+    # run(task2, min_a=234_185_182_108_000, max_a=234_185_182_408_000 + 200_000, delta=5, matches_to_print=10) # 10
+    # again, narrow the sectors (2)
+    # no progress between min_a=216592050808000 and max_a=216592052908000: delta=2100000
+    # no progress between min_a=216592050808000 and max_a=216592052908000: delta=2100000
+    # ...
+    # run only where 11 matches found
+    # run(task2, min_a=216_592_996_171_000, max_a=216_592_996_205_000, delta=1, matches_to_print=12, counter_to_pint_matches=1)
+    # run(task2, min_a=234_185_182_213_000, max_a=234_185_182_253_000, delta=1, matches_to_print=12, counter_to_pint_matches=1)
+    # nothing found :( only 12 matches max - best result is 234185182253000
+
+    # 2. find more precisely another zones with 4 matches
+    # for start, end in [
+    #     (216050542908000, 216250542908000),
+    #     (218250542908000, 218450542908000),
+    #     (233650542908000, 233850542908000),
+    #     (235850542908000, 236050542908000),
+    #     (236450542908000, 236650542908000),
+    # ]:
+    #     run(task2, min_a=start, max_a=end, delta=1_000_000, matches_to_print=5)
+    # ok from last region I found 6 matches, inspect them...damn I lost end :) search again...
+    # run(task2, min_a=236450542908000, max_a=236650542908000, delta=1_000_000, matches_to_print=5)
+    # run(task2, min_a=236_530_542_908_000, max_a=236_600_542_908_000, delta=100_000, matches_to_print=6) # ook found 7
+    # run(task2, min_a=236_547_542_908_000, max_a=236_548_442_908_000, delta=10_000, matches_to_print=7) # ook found 7
+    # found 2 regions - 7 and 9
+    # run(task2, min_a=236_547_542_908_000, max_a=236_547_742_908_000, delta=1_000, matches_to_print=8) # 7 is max =(
+    # run(task2, min_a=236_548_242_908_000, max_a=236_548_302_908_000, delta=100, matches_to_print=10) # 9 and 11 found
+    # take 2 regions - with 9 and 11 - from last region
+    # run(task2, min_a=236_548_279_908_000, max_a=236_548_283_908_000, delta=100, matches_to_print=10) # still 9
+    # run(task2, min_a=236_548_286_908_000, max_a=236_548_289_908_000, delta=10, matches_to_print=11) # still 11
+
+    run(
+        task2, min_a=236_548_287_608_000, max_a=236_548_287_908_000, delta=1, matches_to_print=12
+    )  # FOUND 236548287712877
+    run(task2, min_a=236_548_288_708_000, max_a=236_548_288_808_000, delta=1, matches_to_print=12)  #
